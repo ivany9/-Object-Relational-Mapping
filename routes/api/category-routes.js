37dabc82f38a94989629d30b58ad86dede09cdaf
ivan <ivany9@gmail.com>
2021-08-29 21:03:45 +1000

    delete category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,12 +42,37 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+router.put('/:id', async (req, res) => {
+  try {
+    const categorydata = await Category.update({
+      where:{
+      id:req.params.id,
+      },  
+    });
+    if(!categorydata){
+      res.status(404).json({message:'No Categories found with that id!'});
+      return;
+     }
+     res.status(200).json(categorydata);
+   } catch(err){
+     res.status(500).json(err);
+   }
+ });
+router.delete('/:id', async (req, res) => {
+  try {
+    const categorydata = await Category.destroy({
+      where:{
+      id:req.params.id,
+      },  
+    });
+    if(!categorydata){
+      res.status(404).json({message:'No Categories found with that id!'});
+      return;
+     }
+     res.status(200).json(categorydata);
+   } catch(err){
+     res.status(500).json(err);
+   }
+ });
 
 module.exports = router;
