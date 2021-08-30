cfb55fbedc9c58b7da3ecd96a8454c8fd5e51543
ivan <ivany9@gmail.com>
2021-08-30 21:57:06 +1000

    tag delete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,12 +42,40 @@ router.post('/', async (req, res) => {
 });
 
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagdata = await Tag.update({
+      tag_name: req.body.tag_name},
+      {
+      where:{
+      id:req.params.id,
+      },  
+    });
+    if(!tagdata){
+      res.status(404).json({message:'No Categories found with that id!'});
+      return;
+     }
+     res.status(200).json(tagdata);
+   } catch(err){
+     res.status(500).json(err);
+   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagdata = await Tag.destroy({
+      where:{
+      id:req.params.id,
+      },  
+    });
+    if(!tagdata){
+      res.status(404).json({message:'No Categories found with that id!'});
+      return;
+     }
+     res.status(200).json(tagdata);
+   } catch(err){
+     res.status(500).json(err);
+   }
 });
 
 module.exports = router;
